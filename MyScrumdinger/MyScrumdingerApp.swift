aab0c1f17b4263e4fb1bfb5f1596d643e3907d51
Esoe B.Fildine <myebstudios@gmail.com>
2024-03-22 13:12:45 +0100

    Initial Commit

---
+++ MyScrumdinger/MyScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyScrumdingerApp.swift
+//  MyScrumdinger
+//
+//  Created by Esoe Fildine on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
